22cc8494a2dc17bcb1e2e836a4aa21bd481af6e6
Kevin Sanjaya <kevinsanjayanl@gmail.com>
2019-06-10 14:50:17 +0200

    feat: 🎸 WIP: Add schedules functionality

--- src/views/Schedules.vue
+++ src/views/Schedules.vue
@@ -1,17 +1,63 @@
 <template>
 <div id="schedules">
-    {{ title }}
+    <DivisionSelector v-bind:divisionList="divisionList" @selectDivision="selectDivision" />
+    <ScrimSchedules  v-bind:selectedDivision="selectedDivision"
+                     v-bind:scrimmageSchedules="scrimmageSchedules" />
+    <TournamentSchedules  v-bind:selectedDivision="selectedDivision"
+                          v-bind:tournamentSchedules="tournamentSchedules" />
 </div>
 </template>
 
+<style scoped>
+#schedules {
+  margin: 1%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+</style>
+
 <script>
+import DivisionSelector from '@/components/divisions/DivisionSelector.vue';
+import ScrimSchedules from '@/components/schedules/ScrimSchedules.vue';
+import TournamentSchedules from '@/components/schedules/TournamentSchedules.vue';
+import * as mockDivisions from '../assets/mocks/divisions/divisions.json';
+import * as mockScrimmages from '../assets/mocks/schedules/scrimmages.json';
+import * as mockTournaments from '../assets/mocks/schedules/tournaments.json';
+
 export default {
   name: 'Schedules',
-  components: {},
+  components: {
+    ScrimSchedules,
+    TournamentSchedules,
+    DivisionSelector,
+  },
   data() {
     return {
-      title: 'Schedules',
+      selectedDivision: 'Dota 2',
+      divisionList: [],
+      scrimmageSchedules: [],
+      tournamentSchedules: [],
     };
+  },
+  methods: {
+    selectDivision(selectedDivision) {
+      this.selectedDivision = selectedDivision;
+    },
+    setDivisionList(divisionList) {
+      this.divisionList = divisionList;
+    },
+    setScrimmageSchedules(scrimmageSchedules) {
+      this.scrimmageSchedules = scrimmageSchedules;
+    },
+    setTournamentSchedules(tournamentSchedules) {
+      this.tournamentSchedules = tournamentSchedules;
+    },
+  },
+  mounted() {
+    setTimeout(() => this.setDivisionList(mockDivisions.default.divisions), 2000);
+    setTimeout(() => this.setScrimmageSchedules(mockScrimmages.default.scrimmages), 2000);
+    setTimeout(() => this.setTournamentSchedules(mockTournaments.default.tournaments), 2000);
   },
 };
 </script>
